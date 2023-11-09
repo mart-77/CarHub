cad369b6e2636557d6a471b3dadd6821d1a0cc2b
Martin Rodriguez <martin.rodriguez@upa.edu.py>
2023-11-09 13:30:44 -0300

    Actializacion FAQ

--- app/src/main/java/com/example/myapplication/activities/AcercaDeActivity.java
+++ app/src/main/java/com/example/myapplication/activities/AcercaDeActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +16,18 @@ public class AcercaDeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_acerca_de);
     }
 
+    public void lanzarFAQ(View view) {
+        Intent lanzar = new Intent(this, FAQActivity.class);
+        startActivity(lanzar);
+    }
 
+    public void lanzarChat (View view) {
+        Intent lanzar = new Intent(this, ChatActivity.class);
+        startActivity(lanzar);
+    }
+
+    public void lanzarMain (View view) {
+        Intent lanzar = new Intent(this, MenuPrincipalActivity.class);
+        startActivity(lanzar);
+    }
 }
